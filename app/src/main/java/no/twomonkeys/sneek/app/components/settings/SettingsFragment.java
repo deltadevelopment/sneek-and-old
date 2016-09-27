30e4a0be967d733e7fd01dfa91efe76c0880f1a9
sflovik <sondrf14@gmail.com>
2016-09-27 11:56:49 +0200

    initial commit of GUI rework

---
+++ app/src/main/java/no/twomonkeys/sneek/app/components/settings/SettingsFragment.java
@@ -0,0 +1,36 @@
+package no.twomonkeys.sneek.app.components.settings;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import no.twomonkeys.sneek.R;
+import no.twomonkeys.sneek.app.shared.Callback;
+import no.twomonkeys.sneek.app.shared.SimpleCallback;
+import no.twomonkeys.sneek.app.shared.SimpleCallback2;
+/**
+ * Created by Sondre on 27.09.2016.
+ */
+public class SettingsFragment extends android.support.v4.app.Fragment  {
+    public TextView settingsTitle;
+    public TextView settingsBack;
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.friends_top_fragment, container, false);
+        settingsTitle = (TextView) view.findViewById(R.id.settingsTitleBtn);
+        settingsBack = (TextView) view.findViewById(R.id.settingsBackBtn);
+
+        return view;
+    }
+}
